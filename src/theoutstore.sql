c81f98f042a7aafd1fc41d652e940fe5f564d827
Juan Rocha <Juan Rocha@DESKTOP-J8J5AC2>
2020-11-26 15:13:24 +0100

    Commit inicial

--- src/theoutstore.sql
+++ src/theoutstore.sql
@@ -22,20 +22,24 @@ PRIMARY KEY (clave)
 
 CREATE TABLE productos(
 clave bigint AUTO_INCREMENT NOT NULL,
-nombre varchar(50) NOT NULL,
+nombrep varchar(50) NOT NULL,
 precioUnitario decimal(16,2) NOT NULL,
 id_proveedor bigint NOT NULL,
 costo decimal(16,2) NOT NULL,
+nombre_proveedor varchar(50) NOT NULL,
+stock int NOT NULL,
 PRIMARY KEY (clave)
 );
 
 CREATE TABLE usuarios(
 clave bigint AUTO_INCREMENT NOT NULL,
 nombre varchar(50) NOT NULL,
+usuario varchar(50) NOT NULL,
 email varchar(50) NOT NULL,
 ciudad varchar(50) NOT NULL,
 rfc varchar(50) NOT NULL,
-usuario varchar(50) NOT NULL,
+tipo varchar(50) NOT NULL,
+pass varchar(50) NOT NULL,
 PRIMARY KEY (clave)
 );
 
@@ -56,6 +60,15 @@ cantidad int NOT NULL,
 PRIMARY KEY (clave)
 );
 
+CREATE TABLE clientes(
+clave bigint AUTO_INCREMENT NOT NULL,
+nombre varchar(50) NOT NULL,
+email varchar(50) NOT NULL,
+ciudad varchar(50) NOT NULL,
+rfc varchar(50),
+PRIMARY KEY (clave)
+);
+
 ALTER TABLE concepto
 ADD FOREIGN KEY (id_venta) REFERENCES venta(clave),
 ADD FOREIGN KEY (id_producto) REFERENCES productos(clave);
@@ -70,5 +83,17 @@ ALTER TABLE carrito
 ADD FOREIGN KEY (id_usuario) REFERENCES usuarios(clave),
 ADD FOREIGN KEY (id_producto) REFERENCES productos(clave);
 
+CREATE VIEW vista_CarritoProdutos AS
+SELECT carrito.id_producto, carrito.id_usuario,  productos.nombrep, usuarios.nombre, cantidad FROM carrito
+INNER JOIN productos
+ON carrito.id_producto = productos.clave
+INNER JOIN usuarios
+ON carrito.id_usuario = usuarios.clave;
 
+select * from proveedores;
+select * from usuarios;
+select * from clientes;
+select * from productos;
+select * from carrito;
 
+select * from vista_CarritoProdutos;
